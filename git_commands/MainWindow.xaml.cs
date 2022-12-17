6060e5e979eb8d918063aeeb2c5fee178e1b975e
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-17 12:53:33 +0200

    Create Main window layout.

--- git_commands/MainWindow.xaml.cs
+++ git_commands/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace git_commands
         {
             InitializeComponent();
         }
+
+        private void CloneCommandBtnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
