3d72355a06c71b03f88a404f3163df655dccd728
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-17 13:01:02 +0200

    Add window with clone command description.

--- git_commands/CloneCommand.xaml.cs
+++ git_commands/CloneCommand.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace git_commands
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CloneCommand.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CloneCommand : Window
     {
-        public MainWindow()
+        public CloneCommand()
         {
             InitializeComponent();
-        }
-
-        private void CloneCommandBtnClick(object sender, RoutedEventArgs e)
-        {
-            CloneCommand window = new CloneCommand();
-            window.Show();
         }
     }
 }
