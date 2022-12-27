454d7b4c112854f72dc27d935a90cf63cdcb7946
Alex <oleksii120@gmail.com>
2022-12-27 18:47:54 +0200

    Added BranchCommand window

---
+++ git_commands/BranchCommand.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace git_commands
+{
+    /// <summary>
+    /// Interaction logic for BranchCommand.xaml
+    /// </summary>
+    public partial class BranchCommand : Window
+    {
+        public BranchCommand()
+        {
+            InitializeComponent();
+        }
+    }
+}
